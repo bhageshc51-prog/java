428e931909f04765ff159d761ba399b8eac1b529
Bhagesh.c <bhageshc51@gmail.com>
2026-08-23 17:55:37 +0530

    Add Java program to calculate sum of two integers

---
+++ variable.in.java/day2.java
@@ -0,0 +1,11 @@
+public class bhagesh {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        // variable
+        int a = 20;
+        int b = 25;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
+
