997bf11aa5169c9aa73c0763e682c61d78f3de7d
Bhagesh.c <bhageshc51@gmail.com>
2026-08-23 18:45:01 +0530

    Add Java variables and basic program structure
    Added comments to explain Java variables and program structure.

--- variable.in.java/day2.java
+++ variable.in.java/day2.java
@@ -9,3 +9,43 @@ public class bhagesh {
     }
 }
 
+
+// Java Variables and Basic Program Structure
+public class Bhagesh {
+    // public means the class can be accessed from anywhere.
+    // class is a keyword used to create a class.
+    // Bhagesh is the name of the class.
+
+    public static void main(String[] args) {
+        // public means the main method can be accessed by the JVM.
+        // static allows the main method to run without creating an object.
+        // void means the main method does not return any value.
+        // main is the starting point of a Java program.
+        // String[] args stores command-line arguments as an array of Strings.
+
+        System.out.println("Hello World");
+        // System refers to Java's built-in System class.
+        // out represents the standard output stream.
+        // println() prints the given text and moves to the next line.
+        // "Hello World" is the text that will be displayed.
+
+        // variable
+        // This comment indicates that the following lines create variables.
+
+        int a = 20;
+        // int is the data type used to store whole numbers.
+        // a is the variable name that stores the value 20.
+
+        int b = 25;
+        // int is the data type used to store whole numbers.
+        // b is the variable name that stores the value 25.
+
+        int sum = a + b;
+        // sum is an integer variable that stores the result of adding a and b.
+        // a + b means 20 + 25, which gives 45.
+
+        System.out.println(sum);
+        // println() displays the value stored in the sum variable.
+        // The output will be 45.
+    }
+}
