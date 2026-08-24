8caace8aa571affdf2841d8f227646cc56fb5e9c
Bhagesh.c <bhageshc51@gmail.com>
2026-08-25 00:59:43 +0530

    Implement user input for name using Scanner
    Added user input functionality using Scanner class.

--- variable.in.java/variable.java
+++ variable.in.java/variable.java
@@ -8,3 +8,29 @@ public class variable {
     }
 }
 
+import java.util.*;
+// Imports the Scanner class from java.util.
+// Scanner is used to take input from the user.
+
+public class variable {
+// Defines a class named variable.
+
+    public static void main(String[] args) {
+// main() is the starting point of the Java program.
+
+        Scanner sc = new Scanner(System.in);
+// Creates a Scanner object named sc.
+// System.in takes input from the keyboard.
+
+        String name = sc.next();
+// String is the data type.
+// name is the variable name.
+// sc.next() reads one word entered by the user.
+// The entered value is stored in name.
+
+        System.out.println(name);
+// println() prints the value stored in name.
+// Therefore, the entered name will be displayed.
+    }
+}
+
