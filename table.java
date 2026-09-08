efbfd71ee577f3b995c1774807d59819f95c4014
Bhagesh.c <bhageshc51@gmail.com>
2026-09-08 11:00:33 +0530

    Add multiplication table program in day3Table class

---
+++ table.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class day3Table {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.println(num + " * " + i + " = " + (num * i));
+            i++;
+        }
+    }
+}
