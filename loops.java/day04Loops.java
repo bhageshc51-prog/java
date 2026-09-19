742d54f3b5969952abd0d5c989ec0dfaa6ea685b
Bhagesh.c <bhageshc51@gmail.com>
2026-09-20 00:14:07 +0530

    Implement for loop to print numbers from 0 to 10
    Added a Java program that prints numbers from 0 to 10 using a for loop.

---
+++ loops.java/day04Loops.java
@@ -0,0 +1,34 @@
+// Write a Java program using a for loop to print numbers
+// from 0 to 10.
+
+public class day3Loops {
+    public static void main(String args[]) {
+
+        // i is the loop variable.
+        // i starts from 0.
+
+        // Initialization → i = 0
+        // Condition → i < 11
+        // Increment → i++
+
+        for(int i = 0; i < 11; i++) {
+
+            // println() prints the number
+            // and moves to the next line.
+            System.out.println(i);
+        }
+    }
+}
+
+// Output:
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
